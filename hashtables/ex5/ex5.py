789304127ce9c0df6ba8c5ffdc68b14015820c36
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-08-07 09:54:28 -0700

    finished ex 5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,12 +1,34 @@
-# Your code here
-
-
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
+
+# Given a list of full paths to files, and a list of filenames to query,
+# report all the full paths that match that filename.
+
+    # Empty list
+    result = []
+    # Empty ht for pathsa
+    cache = {}
+
+    # Split the paths on '/', to get the key as the filename
+    for x in files:
+        # Use rsplit instead to begin splitting from right end of the string
+        key = x.rsplit('/', 1)[1] 
+        value = x
+
+        # Create new entry in ht if needed
+        if key not in cache:
+            cache[key] = []
+        # Add current path to cache 
+        cache[key].append(x)
+
+    # For each filename in queries, check to see if it is a key in the ht
+    for y in queries:
+        if y in cache:
+            # If the file is found, add to the path
+            result += cache[y]
 
     return result
 
