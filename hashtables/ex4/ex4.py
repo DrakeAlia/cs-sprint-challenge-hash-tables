7f7f9db7be0333f50fc6a28446abe6eb6e8484b9
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-08-07 09:48:17 -0700

    finished ex 4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,6 +4,25 @@ def has_negatives(a):
     """
     # Your code here
 
+# For an input list of integers, we wish to know which positive numbers
+# have corresponding negative numbers in the list.
+
+# Return value can be in any order.
+# Solve this problem with a hash table.
+
+    # Empty list
+    result = []
+    # Empty hash table
+    cache = {}
+
+    for num in a:
+        # Add int to ht
+        cache[num] = 1
+    for num in a:
+        # If int * -1 is in the ht and if positive int exists, add int to the result array
+        if (num*-1) in cache and num > 0:
+            result.append(num)
+
     return result
 
 
